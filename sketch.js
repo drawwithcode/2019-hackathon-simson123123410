110933d1877382b75f3a2e2377ab66bd32646d02
simsonchen <simson123123410@gmail.com>
2019-11-27 12:58:51 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,56 @@
-function preload(){
-  // put preload code here
+
+
+
+  var analyzer;
+
+   analyzer = new p5.Amplitude();
+   analyzer.setInput(mySong);
+
+
+
+
+
+
+var mySong;
+var myImage;
+
+function preload() {
+  mySong = loadSound("TG1_bumper.mp3");
+  myImage = loadImage('./TG1.png');
+  // myImage2 = loadImage('./Earth.jpg');
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
 }
 
-function draw() {
-  // put drawing code here
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
+
+
+function draw() {
+let rms = analyzer.getLevel();
+  var distx=abs(mouseX-width/2);
+  var disty=abs(mouseY-height/2);
+  if (distx<90 &&disty<67.5) {
+    background(100,170,180);
+
+    imageMode(CENTER);
+    image(myImage,width/2, height/2, 200*(rms+1), 150*(rms+1));
+
+    if (mySong.isPlaying() == false) {
+      mySong.play();
+    }
+  } else {
+    background(130,100,180);
+    imageMode(CENTER);
+    image(myImage,width/2, height/2, 180, 135);
+    mySong.stop();
+  }
+
+    // image(width / 2, height / 2, 10 + rms * 200, 10 + rms * 200);
+}
+// function mousePressed() {
